8ec9c7d6d08831b20e8090b42a8194fa00129142
Reynau5 <reynaumanzor2018@gmail.com>
2025-09-09 21:56:46 -0400

    person2commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,7 +1,8 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("this is the project version 5");
+        System.out.println("this is the project version 6 made by person 2");
+
 
     }
 }
